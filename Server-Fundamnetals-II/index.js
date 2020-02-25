91c014aabd8868ce5a1e36e0f91824cd2dca47d7
Bar Snir <bars@Admins-MacBook-Pro.local>
2020-02-25 11:06:25 +0200

    done with Server-Fundamnetals-II

--- Server-Fundamnetals-II/index.js
+++ Server-Fundamnetals-II/index.js
@@ -1,7 +1,58 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require('body-parser')
 const app = express();
 const PORT = 3000;
 
+const books = [
+  {name: "Name of the Wind", author: "Patrick Rothfuss"}, 
+  {name: "Of Mice and Men", author: "John Steinbeck"}
+]
+
+app.use(bodyParser.json())
+
+
+app.get("/sanity", (req,res,next)=> {
+    responseMsg = "Ok";
+    try {
+      res.send(responseMsg).status(200).end();
+    } catch(e) {
+      throw e;
+    }
+});
+
+app.get("/books", (req,res,next)=> {
+  try {
+    res.send(books).status(200).end();
+  } catch(e) {
+    throw e;
+  }
+});
+
+app.get("/book/:index", (req,res,next)=> {
+  try {
+    res.send(books[req.params.index]).status(200).end();
+  } catch(e) {
+    throw e;
+  }
+});
+
+
+app.post("/book", (req,res,next)=> {
+    const {name, author} = req.body;
+    try {
+      books.push({
+        name: name, 
+        author: author
+      });
+      const actionMsg = "Book added successfully.";
+      res.send(actionMsg).status(200).end();
+    } catch(e) {
+      throw e;
+    }
+    next();
+});
+
 app.listen(PORT,()=>{
-  console.log('app is here');
+  let serverMsg = "Server is up in port"
+  console.log(`${serverMsg} ${PORT}`);
 });
